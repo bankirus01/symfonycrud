3e34e4bd1565b87e50b2d7a038070403665e099c
bankirus01 <bankirus01@gmail.com>
2017-12-12 18:19:19 +0200

    add tests DefaultControllerTest.php and ArticleTest.php

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -10,9 +10,58 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/article');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }
+
+
+    public function testCreateAction()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/article/create');
+        $Nametest = $crawler->filter('div#article > div > label')->text();
+        $this->assertContains('Name', $Nametest); //ok
+        $descriptionTest = $crawler->filter('div#article > div > label')
+            ->eq(1)
+            ->text();
+        $this->assertContains('Description', $descriptionTest);
+        $createdAtTest = $crawler->filter('div#article > div > label')
+            ->eq(2)
+            ->text();
+        $this->assertContains('Created at', $createdAtTest);
+        $form = $crawler->selectButton('Submit')->form();
+        $form['article[name]'] = 'hello test';
+        $form['article[description]'] = 'hello test';
+        $client->submit($form);
+        $crawler = $client->request('GET', '/article');
+        $this->assertContains('hello test', $crawler->filter('body > ul > li')->text());
+        $this->assertContains('hello test', $crawler->filter('body > ul > li')->text());
+    }
+
+    public function testUpdateAction()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/article');
+        $link = $crawler->filter('a:contains("edit")')
+            ->eq(0)
+            ->link();
+        $crawler = $client->click($link);
+        $form = $crawler->selectButton('Submit')->form();
+        $form['article[name]'] = 'change test';
+        $form['article[description]'] = 'change test';
+        $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect('/article'));
+    }
+    public function testDeleteAction()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/article');
+        $link = $crawler->filter('a:contains("delete")')
+            ->eq(0)
+            ->link();
+        $crawler = $client->click($link);
+        $this->assertTrue($client->getResponse()->isRedirect('/article'));
     }
 }
---
+++ tests/AppBundle/Entity/ArticleTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace tests\AppBundle\Entity;
+
+use AppBundle\Entity\Article;
+use PHPUnit\Framework\TestCase;
+
+class ArticleTest extends TestCase
+{
+    public function testId()
+    {
+        $article = new Article();
+        $this->assertEquals(null , $article->getId());
+    }
+
+    public function testName()
+    {
+        $nameTest = 'Test 1';
+        $testArticle = new Article();
+        $testArticle->setName($nameTest);
+        $this->assertEquals($nameTest, $testArticle->getName());
+    }
+
+    public function testDescription()
+    {
+        $DescriptionTest = 'Test 1';
+        $testArticle = new Article();
+        $testArticle->setDescription($DescriptionTest);
+        $this->assertEquals($DescriptionTest, $testArticle->getDescription());
+    }
+
+    public function testCreated()
+    {
+        $createdTest = new \DateTime('2017-01-01 00:00:00');
+        $testArticle = new Article();
+        $testArticle->setCreatedAt($createdTest);
+        $this->assertEquals($createdTest, $testArticle->getCreatedAt());
+    }
+}
